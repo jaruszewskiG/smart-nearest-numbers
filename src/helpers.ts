19be7ecb15f6251fd4114c00f2f4102cf2ec5a1b
Grzegorz Jaruszewski <jaruszewskig@gmail.com>
2019-07-14 18:09:16 +0200

    Add handling float numbers

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,3 +1,15 @@
 export function deepCopy(object: any) {
   return JSON.parse(JSON.stringify(object));
 }
+
+export function handleFloatNumbers(number: number) {
+  if (!Number.isInteger(number)) {
+    let roundedNumber = Math.round(number);
+    let isRoundedNumberHigher = true;
+    if (roundedNumber < number) {
+      isRoundedNumberHigher = false;
+    }
+
+    return { roundedNumber, isRoundedNumberHigher};
+  }
+}
